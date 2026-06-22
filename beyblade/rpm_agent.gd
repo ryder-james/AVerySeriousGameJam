c0a2ba2667453e701ac68dc5e57ca1befa055f39
Ryder James <work.jamesr@gmail.com>
2026-06-21 22:06:16 -0600

    feat: Add indefinite enemy spawning

--- beyblade/rpm_agent.gd
+++ beyblade/rpm_agent.gd
@@ -13,4 +13,7 @@ func get_rpm() -> float:
 	if not parent_rb or not is_instance_valid(parent_rb):
 		return 0.0
 	
-	return floorf(parent_rb.angular_velocity / TAU * 100) * 0.01
+	var rps := parent_rb.angular_velocity / TAU
+	if is_equal_approx(rps, 300):
+		return 300
+	return floorf(rps * 100) * 0.01
