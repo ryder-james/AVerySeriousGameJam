f418c20f948019f1066bf96b6387c3b62ba61e6c
Ryder James <work.jamesr@gmail.com>
2026-06-26 17:03:10 -0600

    feat: Sample sfx vol

--- ui/volume_slider.gd
+++ ui/volume_slider.gd
@@ -3,7 +3,10 @@ extends HBoxContainer
 
 @export var slider_name: String
 @export var bus: StringName
+@export var test_sound: AudioStreamPlayer
 
+const MAX_COUNTDOWN := 0.25
+var _test_countdown := 0.0
 
 @onready var _label: Label = %Label
 @onready var _slider: HSlider = %HSlider
@@ -19,10 +22,17 @@ func _ready() -> void:
 	_slider.value_changed.connect(func(new_value: float) -> void:
 			AudioServer.set_bus_volume_linear(_bus_idx, new_value)
 			_num_label.text = str(new_value * 100.0)
+			if test_sound:
+				_test_countdown = MAX_COUNTDOWN
 	)
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	if _test_countdown <= 0.0:
+		return
+	_test_countdown -= delta
+	if _test_countdown <= 0.0:
+		test_sound.play()
 	if not Engine.is_editor_hint():
 		return
 	
